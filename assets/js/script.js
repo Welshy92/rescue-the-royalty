0f776c154e5b934323c989fe8aa1696697d35921
Jacob Welsh <welshy92@outlook.com>
2022-10-17 16:19:48 +0000

    Add list of functions required for script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,73 @@
-console.log("Link accomplished!");
+// Wait for the DOM to finish loading before perfoming any tasks
+document.addEventListener ("DOMContentLoaded", function() {
+})
+
+let startGame = document.getElementById("start-game");
+startGame.addEventListener("click", function() {
+    console.log("START THE GAME ALREADY!");
+
+})
+
+function battleScreen() {
+    
+}
+
+function selectEnemy() {
+
+}
+
+function displayInstructions() {
+
+}
+
+function playerHP() {
+
+}
+
+function enemyHP() {
+
+}
+
+function attackCalc() {
+
+}
+
+function enemyAttack() {
+
+}
+
+function swordSwing() {
+
+}
+
+function fireAttack() {
+
+}
+
+function waterAttack() {
+
+}
+
+function airAttack() {
+
+}
+
+function groundAttack() {
+
+}
+
+function HealAttack() {
+
+}
+
+function defenceCalc() {
+
+}
+
+function playerVictory() {
+
+}
+
+function playerDefeat() {
+
+}
