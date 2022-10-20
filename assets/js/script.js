a80ddd831f58610534ab9a3311e8b3430f6efa26
Jacob Welsh <welshy92@outlook.com>
2022-10-20 13:42:41 +0000

    add enemy defining functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,10 @@ function handleSubmit(event) {
 let userForm = document.getElementById("user-name");
 userForm.addEventListener("submit", handleSubmit);
 
+// Naming the 2 boxes that will be changed throughout gameplay.
+
 let battleScreen = document.getElementById("game-area");
+let infoScreen = document.getElementById("instructions");
 
 /* This will run at the start of every new fight.
 It resets the game area and sets the enemy the user faces,
@@ -30,12 +33,21 @@ as well as randomising the player stats. */
 
 function battleStart() {
     console.log("BATTLE STARTED!")
+    clearScreen();
     selectEnemy();
     playerStats();
     playerHP = 100;
     enemyHP = 100;
 }
 
+// Clears the game screen
+
+function clearScreen() {
+    console.log("CLEARING BATTLE SCREEN");
+    battleScreen.innerHTML = ``;
+    console.log("CLEAING INFO SCREEN");
+    infoScreen.innerHTML = ``;
+}
 // Selects a random enemy for the user to face.
 
 function selectEnemy() {
@@ -142,3 +154,67 @@ function playerVictory() {
 function playerDefeat() {
     console.log("YOU LOSE")
 }
+
+// Defining enemies
+
+let enemyList = [fireSlime, waterSlime, airSlime, groundSlime, treant, armageddon];
+let weakAttack = [weakWater, weakAir, weakGround, weakFire, weakSword];
+let nullifyAttack = [nullifyFire, nullifyWater, nullifyAir, nullifyGround];
+let resistAttack = [resistGround, resistFire, resistWater, resistAir, resistSword];
+let enemyDamage = [fireDamage, waterDamage, airDamage, groundDamage, slashDamage];
+let normalAttack = [airNormal, groundNormal, fireNormal, waterNormal, slashNormal];
+
+function fireSlime() {
+    enemyName = "Fire Slime";
+    enemyHP = 100;
+    weakAttack[1];
+    nullifyAttack[1];
+    resistAttack[1];
+    enemyDamage[1];
+    normalAttack[1, 5];
+} 
+
+function fireSlime() {
+    enemyName = "Water Slime";
+    enemyHP = 100;
+    weakAttack[2];
+    nullifyAttack[2];
+    resistAttack[2];
+    enemyDamage[2];
+    normalAttack[2, 5];
+} 
+
+function airSlime() {
+    enemyName = "Air Slime";
+    enemyHP = 100;
+    weakAttack[3];
+    nullifyAttack[3];
+    resistAttack[3];
+    enemyDamage[3];
+    normalAttack[3, 5];
+} 
+
+function groundSlime() {
+    enemyName = "Ground Slime";
+    enemyHP = 100;
+    weakAttack[4];
+    nullifyAttack[4];
+    resistAttack[4];
+    enemyDamage[4];
+    normalAttack[4, 5];
+} 
+
+function treant() {
+    enemyName = "Anti-magic Treant";
+    enemyHP = 100;
+    resistAttack[1, 2, 3, 4];
+    enemyDamage[5];
+    normalAttack[5];
+} 
+
+function armageddon() {
+    enemyName = "Armageddon";
+    enemyHP = 1000;
+    normalAttack[1, 2, 3, 4, 5];
+    
+} 
